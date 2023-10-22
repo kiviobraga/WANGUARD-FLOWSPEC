a987a3785fb8554d1301a623be46e3cc68c2ca81
kiviobraga <126835454+kiviobraga@users.noreply.github.com>
2023-10-22 13:21:53 -0300

    Update flowspec.sh
    update 22/10/2023

--- flowspec.sh
+++ flowspec.sh
@@ -5,20 +5,21 @@
 if [ $# -lt 3 ]
  then
      echo
-     echo " Usage: $0 <IP> <DECODER> <RATE> <ANOMALY_ID> <GROUP>"
+     echo " Usage: $0 <REDE/IP> <DECODER> <RATE> <UNIT> <ANOMALY_ID> <GROUP> <DIRECTION>"
      echo
-     echo " <DECORDER> = decoder=UDP | decoder=ICMP | decoder=NTP | decoder=SNMP | decoder=SSDP | decoder=CLDAP | decoder=INVALID | decoder=FRAGMENT | decoder=OTHER"
+     echo " <DECORDER> = decoder=IP | decoder=UDP | decoder=ICMP | decoder=NTP | decoder=SNMP | decoder=SSDP | decoder=CLDAP | decoder=INVALID | decoder=FRAGMENT | decoder=OTHER"
      echo " <RATE> = 1000000"
-     echo " <ANOMALY_ID> = id=43"
+     echo " <UNIT> = unit=bits/s | unit=pkts/s"
+     echo " <ANOMALY_ID> = id=1"
      echo " <GROUP> = group=WANGUARD"
      echo " <DIRECTION> = direction=incoming | direction=outgoing"
      echo
-     echo " Exemplo_0: $0 <REDE>/24 decoder=UDP rate=1000000 direction=incoming"
-     echo " Exemplo_1: $0 <IP>/32 decoder=ICMP rate=1000000 direction=incoming"
-     echo " Exemplo_2: $0 <IP>/32 decoder=NTP rate=1000000 direction=incoming"
-     echo " Exemplo_3: $0 <IP>/32 decoder=SNMP rate=2000000 direction=outgoing"
-     echo " Exemplo_4: $0 <REDE>/24 decoder=FRAGMENT rate=1000000 direction=outgoing"
-     echo " Exemplo_5: $0 <REDE>/24 decoder=OTHER rate=1000000 direction=outgoing"
+     echo " Exemplo_0: $0 <REDE>/24 decoder=UDP rate=1000000 unit=bits/s id=1 group=WANGUARD direction=incoming"
+     echo " Exemplo_1: $0 <IP>/32 decoder=ICMP rate=1000000 unit=bits/s id=1 group=WANGUARD direction=incoming"
+     echo " Exemplo_2: $0 <IP>/32 decoder=NTP rate=1000000 unit=bits/s id=1 group=WANGUARD direction=incoming"
+     echo " Exemplo_3: $0 <IP>/32 decoder=SNMP rate=2000000 unit=bits/s id=1 group=WANGUARD direction=outgoing"
+     echo " Exemplo_4: $0 <REDE>/24 decoder=FRAGMENT rate=1000000 unit=bits/s id=1 group=WANGUARD direction=outgoing"
+     echo " Exemplo_5: $0 <REDE>/24 decoder=OTHER rate=1000 unit=pkts/s id=1 group=WANGUARD direction=outgoing"
   exit 2
 fi
 
@@ -39,78 +40,66 @@ URL="-X POST http://127.0.0.1/wanguard-api/v1/bgp_announcements --user $USER_API
 CONNECTOR_ID="2"
 LOG="/var/log/flowspec.log"
 DATE=$(date "+%Y-%m-%d %H:%M:%S")
-
-if [ ! -f "$LOG" ]; then
-touch $LOG
-chown andrisoft:andrisoft $LOG
-fi
-
+[ ! -e "$LOG" ] && touch $LOG && chown andrisoft:andrisoft $LOG
 
 # CHECK_UNIT
 if [ "$UNIT" = "pkts/s" ]
 then
-echo "$DATE - FLOWSPEC_FAILED: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP] - nao e possivel criar regras pkts/s !" | stdbuf -oL tee -a $LOG
-exit 0
+	RATE=$(echo $3 | cut -d= -f2)
+	BPS=$(echo "$RATE * 1500 * 8" | bc)
+	MBPS=$(echo "scale=2; ${BPS} / 1000000" | bc)
+	MBPS=$(echo "${MBPS}M")
+	RATE=$(echo ${BPS} | sed s/.$//)
+elif [ "$UNIT" = "bits/s" ]
+then
+	RATE=$(echo $3 | cut -d= -f2 | sed s/.$//)
+	MBPS=$(echo "$(( ${RATE} / 100000))M")
+else
+	echo "$DATE - FLOWSPEC_FAILED: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP] - Parametro invalido !" | stdbuf -oL tee -a $LOG
+	exit 0
 fi
+
 
 # CHECK_DIRECTION
 if [ "$DIRECTION" = "incoming" ]
 then
-DIRECTION="destination_prefix"
+	DIRECTION="destination_prefix"
 elif [ "$DIRECTION" = "outgoing" ]
 then
-DIRECTION="source_prefix"
+	DIRECTION="source_prefix"
 else
-echo "$DATE - FLOWSPEC_FAILED: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP] | DIRECTION=[$DIRECTION] - falta de parametro!" | stdbuf -oL tee -a $LOG
-exit 0
+	echo "$DATE - FLOWSPEC_FAILED: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP] | DIRECTION=[$DIRECTION] - falta de parametro!" | stdbuf -oL tee -a $LOG
+	exit 0
 fi
 
-# CHECK_PORT
+# CHECK_DECODER
 if [ "$DECODER" = "DNS" ]
 then
-PORT="53"
+	PORT="53"
 elif [ "$DECODER" = "SNMP" ]
 then
-PORT="161"
+	PORT="161"
 elif [ "$DECODER" = "NTP" ]
 then
-PORT="123"
+	PORT="123"
 elif [ "$DECODER" = "SSDP" ]
 then
-PORT="1900"
+	PORT="1900"
 elif [ "$DECODER" = "CLDAP" ]
 then
-PORT="389"
+	PORT="389"
 elif [ "$DECODER" = "CHARGEN" ]
 then
-PORT="19"
+	PORT="19"
 elif [ "$DECODER" = "MEMCACHED" ]
 then
-PORT="11211"
+	PORT="11211"
 elif [ "$DECODER" = "INVALID" ]
 then
-PORT="0"
+	PORT="0"
 elif [ "$DECODER" = "OTHER" ]
 then
-PROTOCOL="\"IP-in-IP\",\"EGP\",\"GRE\",\"ESP\",\"EIGRP\",\"VRRP\""
-elif [ "$DECODER" = "TCP+SYN" ]
-then
-TCP_FLAGS="\"syn\""
-elif [ "$DECODER" = "TCP+ACK" ]
-then
-TCP_FLAGS="\"ack\""
-elif [ "$DECODER" = "TCP+RST" ]
-then
-TCP_FLAGS="\"rst\""
-elif [ "$DECODER" = "TCP+SYNACK" ]
-then
-TCP_FLAGS="\"syn\",\"ack\""
-elif [ "$DECODER" = "TCP-ALL" ]
-then
-TCP_FLAGS="\"syn\",\"ack\",\"fin\",\"rst\",\"push\",\"urgent\""
-elif [ "$DECODER" = "TCP-NULL" ]
-then
-TCP_FLAGS="\"syn\",\"ack\",\"fin\",\"rst\",\"push\",\"urgent\""
+	PROTOCOL="\"IP-in-IP\",\"EGP\",\"GRE\",\"ESP\",\"EIGRP\",\"VRRP\""
 fi
 
 
@@ -164,31 +153,6 @@ curl $URL -H "Content-Type:application/json" -H "Accept:application/json" --data
 echo "$DATE - FLOWSPEC_ADD: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP]" | stdbuf -oL tee -a $LOG
 exit 0
 
-elif [ "$DECODER" = "TCP-NULL" ] || [ "$DECODER" = "TCP-ALL" ] || [ "$DECODER" = "TCP+SYN" ] || [ "$DECODER" = "TCP+ACK" ] || [ "$DECODER" = "TCP+SYNACK" ] || [ "$DECODER" = "TCP+RST" ]; then
-
-generate_ratelimit_tcp_flags()
-{
-cat << EOF
-{
-     "flowspec announcement":	{
-			"bgp_connector_id":"$CONNECTOR_ID",
-			"ip_protocol(s)":["TCP"],
-			"${DIRECTION}":"$IP",
-                        "tcp_flag(s)":[$TCP_FLAGS],
-			"action":"Rate Limit",
-			"rate_limit":"$RATE",
-			"anomaly_id":"$ANOMALY_ID",
-                        "withdraw_after":"$TIMER_WITHDRAW",
-			"comments":"${GROUP} | ${DECODER} | RATE_${MBPS}"
-     }
-}
-EOF
-}
-
-curl $URL -H "Content-Type:application/json" -H "Accept:application/json" --data-binary "$(generate_ratelimit_tcp_flaps)"
-echo "$DATE - FLOWSPEC_ADD: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP]" | stdbuf -oL tee -a $LOG
-exit 0
-
 elif [ "$DECODER" = "INVALID" ]; then
 
 generate_ratelimit_invalid()
@@ -236,6 +200,30 @@ EOF
 }
 
 curl $URL -H "Content-Type:application/json" -H "Accept:application/json" --data-binary "$(generate_ratelimit_fragment)"
+echo "$DATE - FLOWSPEC_ADD: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP]" | stdbuf -oL tee -a $LOG
+exit 0
+
+elif [ "$DECODER" = "IP" ]; then
+
+generate_ratelimit_ip()
+{
+cat << EOF
+{
+     "flowspec announcement":   {
+                        "bgp_connector_id":"$CONNECTOR_ID",
+                        "ip_protocol(s)":[],
+                        "${DIRECTION}":"$IP",
+                        "action":"Rate Limit",
+                        "rate_limit":"$RATE",
+                        "anomaly_id":"$ANOMALY_ID",
+                        "withdraw_after":"$TIMER_WITHDRAW",
+                        "comments":"${GROUP} | ${DECODER} | RATE_${MBPS}"
+     }
+}
+EOF
+}
+
+curl $URL -H "Content-Type:application/json" -H "Accept:application/json" --data-binary "$(generate_ratelimit_ip)"
 echo "$DATE - FLOWSPEC_ADD: ANOMALIA=[$ANOMALY_ID] | PREFIX=[$IP] | DECODER=[$DECODER] | RATE=[$RATE] | UNIT=[$UNIT] | GROUP=[$GROUP]" | stdbuf -oL tee -a $LOG
 exit 0
 
